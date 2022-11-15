3c1dd3657edcf5fcc924eae4e653f4ed33956764
mish-hash <poluektov23@gmail.com>
2022-11-15 23:08:40 +0200

    Clear comments

--- src/components/Goose.jsx
+++ src/components/Goose.jsx
@@ -1,19 +1,10 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import styles from './Goose.module.scss';
-// import LoaderGoose from './LoaderGoose/LoaderGoose';
-// import StartGoose from './StartGoose/StartGoose';
 import SelectTarget from './SelectTarget/SelectTarget';
 import TargetResult from './TargetResult/TargetResult';
 import { PageContext } from '../providers/PageProvider';
 import { DataContext } from '../providers/DataProvider';
 import StartPage from './StartPage/StartPage';
-
-// function StartPage() {
-//     const [isLoad, setIsLoad] = useState(true);
-//     setTimeout(()=>{setIsLoad(false)}, 1500);
-
-//     return <>{isLoad ? <LoaderGoose/> : <StartGoose/>}</>;
-// };
 
 function Goose () {
 
