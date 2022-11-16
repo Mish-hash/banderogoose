fd27f06e599b6609a981e3f32f75fe342cb6bc8f
mish-hash <poluektov23@gmail.com>
2022-11-16 14:13:01 +0200

    Add hendler click on close icon

--- src/components/UI/TargetWindow/TargetWindow.jsx
+++ src/components/UI/TargetWindow/TargetWindow.jsx
@@ -1,14 +1,24 @@
 import styles from './TargetWindow.module.scss';
 import closeIcon from '../../img/close.webp';
 import Button from '../Button/Button';
+import { useContext } from 'react';
+import { PageContext } from '../../../providers/PageProvider';
+import { DataContext } from '../../../providers/DataProvider';
 
 function TargetWindow(props) {
 
+    const { pages } = useContext(DataContext);
+    const { setPage } = useContext(PageContext);
+
     const { headerText, descriptionText, buttonText } = props;
+
+    const onCloseClick = () => {
+        setPage(pages.startPage)
+    };
 
     return(
         <div className={styles.container}>
-            <div className={styles.close}><img src={closeIcon} alt="close-icon" /></div>
+            <div className={styles.close}><img src={closeIcon} alt="close-icon" onClick={onCloseClick}/></div>
             <div className={styles.header}><p>{headerText}</p></div>
             <div className={styles.description}><p>{descriptionText}</p></div>
             <div className={styles.targets}>
